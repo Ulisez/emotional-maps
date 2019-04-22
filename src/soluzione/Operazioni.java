23e823544914d835e4133a43c75b96c917ada9b7
Ulises Sanchez Perez <ulisezsanchez19@gmail.com>
2019-04-22 17:07:02 +0200

    Realizzazione del metodo handleCommands (Leggo i comandi all'interno del file e li savo all'interno di una struttura di tipo coda solo se tali comandi rispettano il formato desiderato, per controllare se il formato è corretto lo controllo attraverso il metodo ValidationFormatCommand

--- src/soluzione/Operazioni.java
+++ src/soluzione/Operazioni.java
@@ -1,47 +1,45 @@
 package soluzione;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+//import java.util.List;
+import java.util.PriorityQueue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Operazioni {
 
 	//public static List<Evento> eventi = new ArrayList<Evento>();
+	public static Queue<String> commandscoda = new LinkedList<String>();
 
 	/*Metodo per la gestione dei comandi
 	Verifico che il file esista se esite leggo i comandi all'interno del file e verifico che ogni 
 	riga del file rappresenti effettivmaente un comando */
 	
 	public static void handleCommands(String fileName) {
-		
-		File file = new File(fileName);
-		if(file.isFile()) {
-			
-		}
-		Scanner scanner = null;
-		if (file.exists()) {
-			try {
-				scanner = new Scanner(file);
-				while (scanner.hasNextLine()) {
-					String commandString = scanner.nextLine();
-					//processCommand(commandString);
-				}
-			} catch (FileNotFoundException e) {
-				/*e.printStackTrace()*/
-				System.out.println("File non esiste");
+		   String commandString;
+		try(BufferedReader readerFile = new BufferedReader(new FileReader(fileName))) {
+		while ((commandString = readerFile.readLine()) !=null) {
+		    System.out.println(commandString);
+		     //processCommand(commandString);
+		    if(validationCommand(commandString)) { 
+		    commandscoda.add(commandString);
+		     }
 			}
-
+		while(!commandscoda.isEmpty()) {
+			System.out.println("");
+			System.out.println(commandscoda.poll());
 		}
-		else
-			System.out.println("Mi trovo qui");
-		}
-	
-
-	private static void processCommand(String commandString) {
+		} catch (Exception e) {
+			 System.err.println(e.getMessage());
+			    }
+			}
+		
+    private static void processCommand(String commandString) {
 		Pattern pattern = null;
 
 		String regexImport = "^import\\((.*?)\\)$";
@@ -65,6 +63,26 @@ public class Operazioni {
 			}
 		}
 
+	}
+	
+	//vedere la funzionalita di split in modo di vedere se è possibile gestire due esprissioni regolari con un'unico oggetto
+    private static boolean validationCommand(String commandString) {
+		boolean validation=false;
+		String regexImport = "^import\\((.*?)\\)$";
+		String regexCreateMap = "^create_map\\((\\d{8}?)-(\\d{8}?)\\)$";
+		Pattern patternImport = Pattern.compile(regexImport);
+		Matcher matcherImport = patternImport.matcher(commandString);
+		Pattern patternCreate = Pattern.compile(regexCreateMap);
+		Matcher matcherCreate = patternCreate.matcher(commandString);
+	
+		if(matcherImport.matches() || matcherCreate.matches()) {
+			validation = true;
+			return validation;
+			}
+		else {
+			System.err.println("La Stringa "+commandString + " non rispetta il formato desiderato");
+			return false;
+		}
 	}
 	
     /*  public static void import(String eventFileName) {
